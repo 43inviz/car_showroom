0f835fbdee56817ecb387625c4fcc90813909868
MaxTeploff <champangekobain@gmail.com>
2024-03-27 20:13:33 +0200

    add main.cpp

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+/*–озрахувати п≥дсумкову варт≥сть покупки, перв≥сну варт≥сть запросити у користувача.
+якщо варт≥сть покупки б≥льша за 1000 грн, але менша за 2000грн, знижка 5%, €кщо б≥льше за 2000 грн, але менша за 5000 грн, знижка 10%.
+*/
+int main() {
+	int user_price,total = 0,sale=0;
+	const int MIN_PR = 1000;
+	const int MID_PR = 2000;
+	const int MAX_PR = 5000;
+	cout << "enter ur price: ";
+	cin >> user_price; 
+	if (user_price > MIN_PR && user_price < MID_PR) {
+		sale = (user_price / 100) * 5;
+		total = user_price - sale;
+		cout << "Total check: " << total << '$';
+
+	}
+	else if(user_price>MIN_PR && user_price < MAX_PR){
+		sale = (user_price / 100) * 10;
+		total = user_price - sale;
+		cout << "Total check: " << total << '$';
+	
+
+	}
+}
